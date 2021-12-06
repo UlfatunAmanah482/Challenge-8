4a883142c7bce7ac0dbe3eb9c96f40c73394d6f8
ulfatun_amanah <ulfaamana@gmail.com>
2021-12-06 14:25:59 +0700

    [menambahkan file swagger.json dan menampilkannya di browser]

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
+const swaggerUi = require("swagger-ui-express")
+const swaggerJSON = require('./app/docs/swagger.json')
+const sequelize = require('./app/models')
 
 var corsOptions = {
   origin: "http://localhost:8081"
@@ -13,6 +16,11 @@ app.use(cors(corsOptions));
 // accept request in form or JSON
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerJSON))
+
+// app.use('/v1', todo)
+
+// sequelize.authenticate().then(() => console.log('db ok'))
 
 const db = require("./app/models");
 db.client.sync();
--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,8 +2,8 @@
 module.exports = {
   HOST: "localhost",
   USER: "postgres",
-  PASSWORD: "password",
-  DB: "postgres",
+  PASSWORD: "ulfa482",
+  DB: "chapter_8",
   dialect: "postgres",
   pool: {
     max: 5,
